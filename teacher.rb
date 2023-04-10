e5023b377cce4641aeefd66b770238e167280439
Habtamu Asmare <habtasm@dtu.edu.et>
2023-04-10 14:35:56 +0300

    Fix teacher file  based on reviewer

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(age, specialization, name: 'Unknown', parent_permission: true)
-    super(age, parent_permission, name)
+    super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
